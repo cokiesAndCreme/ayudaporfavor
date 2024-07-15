7dca9810601823194f029610ba9eba8efbb0fdd7
cokiesAndCreme <martillasoenelano9@gmail.com>
2024-07-15 11:19:20 -0600

    Mensaje del commit inicial

---
+++ src/main/java/com/example/micol/MicolApplication.java
@@ -0,0 +1,13 @@
+package com.example.micol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/micol/MicolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
